7d8a42ac277e4fd3653cc8c98fe5d17549b763fc
Michael Sims <michael.h.sims@gmail.com>
2011-06-11 12:36:08 -0500

    Add a dialog so I can start testing things

--- src/com/example/msims/dialogfragmenttest/DialogFragmentActivity.java
+++ src/com/example/msims/dialogfragmenttest/DialogFragmentActivity.java
@@ -1,7 +1,11 @@
 package com.example.msims.dialogfragmenttest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DialogFragmentActivity extends Activity {
 
@@ -9,5 +13,18 @@ public class DialogFragmentActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button) findViewById(R.id.theButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                showMyDialog();
+            }
+        });
+        showMyDialog();
+    }
+
+    private void showMyDialog() {
+        FragmentManager fm = getFragmentManager();
+        MyDialog myDialog = new MyDialog();
+        myDialog.show(fm, "myDialog");
     }
 }
---
+++ src/com/example/msims/dialogfragmenttest/MyDialog.java
@@ -0,0 +1,48 @@
+package com.example.msims.dialogfragmenttest;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MyDialog extends DialogFragment {
+
+    /*
+     * Order of initialization:
+     * onCreate()
+     * onCreateDialog()
+     * onCreateView()
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d("Flubber", "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.d("Flubber", "onCreateDialog");
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("Flubber", "onCreateView");
+
+        /* Inflate something here */
+        View view = inflater.inflate(R.layout.my_dialog, null, false);
+        Button dismissButton = (Button) view.findViewById(R.id.dismiss_button);
+        dismissButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+}
