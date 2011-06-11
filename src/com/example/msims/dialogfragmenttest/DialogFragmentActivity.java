8f8e31effaf4c742f094fe7f4888c426c6a63727
Michael Sims <michael.h.sims@gmail.com>
2011-06-11 12:14:53 -0500

    Initial revision

---
+++ src/com/example/msims/dialogfragmenttest/DialogFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.msims.dialogfragmenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogFragmentActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
