ea71bdca54cf7021858c6d7098e35c9da1a49abf
Michael Sims <michael.h.sims@gmail.com>
2011-06-11 13:46:13 -0500

    Cleanup import

--- src/com/example/msims/dialogfragmenttest/DialogFragmentActivity.java
+++ src/com/example/msims/dialogfragmenttest/DialogFragmentActivity.java
@@ -5,7 +5,6 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class DialogFragmentActivity extends Activity {
 
